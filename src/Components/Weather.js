b1e273b4a0643386d7a84594fefccb99dd5eeefd
SejanasJoshua <sejanas1joshua@gmail.com>
2021-11-01 08:00:40 +0530

    first commit

---
+++ src/Components/Weather.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+import WeatherCard from "./WeatherCard";
+
+const Weather = () => {
+  const [searchValue, setSearchValue] = useState("Port Blair");
+  const [weatherData, setWeatherData] = useState({});
+
+  const getWeatherInfo = async () => {
+    try {
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=9a735b93d9adbdbf5a553721d51deec8`;
+
+      let urlresponse = await fetch(url);
+      let data = await urlresponse.json();
+
+      const { temp, humidity, pressure } = data.main;
+      const { main: weathermood } = data.weather[0];
+      const { name: cityname } = data;
+      const { speed: windspeed } = data.wind;
+      const { country, sunset } = data.sys;
+
+      const newWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weathermood,
+        cityname,
+        windspeed,
+        country,
+        sunset,
+      };
+      setWeatherData(newWeatherInfo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
+
+  return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="Search..."
+            autoFocus
+            id="search"
+            className="searchTerm"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+          <button
+            className="searchButton"
+            type="button"
+            onClick={getWeatherInfo}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+      <WeatherCard weatherData={weatherData} />
+    </>
+  );
+};
+
+export default Weather;
---
+++ src/Components/WeatherCard.js
@@ -0,0 +1,110 @@
+import { useState, useEffect } from "react";
+
+export default function WeatherCard({ weatherData }) {
+  const [weatherState, setWeatherState] = useState("");
+  const {
+    temp,
+    humidity,
+    pressure,
+    weathermood,
+    cityname,
+    windspeed,
+    country,
+    sunset,
+  } = weatherData;
+  console.log(weatherData);
+
+  //weatherMood
+  useEffect(() => {
+    if (weathermood) {
+      switch (weathermood) {
+        case "Clouds":
+          setWeatherState("wi-cloudy");
+          break;
+        case "Haze":
+          setWeatherState("wi-fog");
+          break;
+        case "Clear":
+          setWeatherState("wi-day-sunny");
+          break;
+        case "Rain":
+          setWeatherState("wi-rain");
+          break;
+
+        default:
+          setWeatherState("wi-day-sunny");
+      }
+    }
+  }, [weathermood]);
+
+  //coverting time
+  let sec = sunset;
+  let date = new Date(sec * 1000);
+  let times = `${date.getHours()}:${date.getMinutes()}`;
+  return (
+    <article className="widget">
+      <div className="weatherIcon">
+        <i className={`wi ${weatherState}`}></i>
+      </div>
+      <div className="weatherInfo">
+        <div className="temperature">
+          <span>{temp}&deg;</span>
+        </div>
+        <div className="description">
+          <div className="weatherCondition">{weathermood}</div>
+          <div className="place">
+            {cityname},{country}
+          </div>
+        </div>
+      </div>
+      <div className="date">{new Date().toLocaleString()}</div>
+
+      <div className="extra-temp">
+        <div className="temp-info-minmax">
+          <div className="two-sided-section">
+            <p>
+              <i className={"wi wi-sunset"}></i>
+            </p>
+            <p className="extra-info-leftside">
+              {times}
+              <br />
+              Sunset
+            </p>
+          </div>
+          <div className="two-sided-section">
+            <p>
+              <i className={"wi wi-humidity"}></i>
+            </p>
+            <p className="extra-info-leftside">
+              {humidity}%
+              <br />
+              Humidity
+            </p>
+          </div>
+        </div>
+        <div className="weather-extra-info">
+          <div className="two-sided-section">
+            <p>
+              <i className={"wi wi-barometer"}></i>
+            </p>
+            <p className="extra-info-leftside">
+              {pressure}(hPa)
+              <br />
+              Pressure
+            </p>
+          </div>
+          <div className="two-sided-section">
+            <p>
+              <i className={"wi wi-windy"}></i>
+            </p>
+            <p className="extra-info-leftside">
+              {windspeed}m/s
+              <br />
+              Windspeed
+            </p>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+}
